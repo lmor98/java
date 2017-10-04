d2dd782aa9c62b6386f9a00d05a0ea1748d57398
Besso <Besso@LAPTOP-DR2VIURC>
2017-10-04 19:30:59 +0200

    Commit inicial

---
+++ src/fecha/Fecha.java
@@ -0,0 +1,26 @@
+package fecha;
+public class Fecha {
+    private int dia;
+    private int mes;
+    private int año;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    public Fecha(int d, int m, int a) {
+        dia = d;
+        mes = m;  
+        año = a;
+
+       /* if (!fechaCorrecta()) {
+            Calendar fechaSistema = Calendar.getInstance();
+            setDia(fechaSistema.get(Calendar.DAY_OF_MONTH));
+            setMes(fechaSistema.get(Calendar.MONTH));
+            setAño(fechaSistema.get(Calendar.YEAR));
+        }*/
+    }
+    
+}
